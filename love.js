fe7afa23eb3e2dc88a40ef3e0e927eadadbbb512
eleanorfmills <eleanor.f.mills@gmail.com>
2017-07-21 08:30:37 -0400

    Create love.js

---
+++ love.js
@@ -0,0 +1,6 @@
+function calculator() {
+var user_input = $("#submit_button").val(); 
+var user_input = Math.floor(Math.random() * 101);
+    alert("Your love compatibility is: " + user_input + "%");
+
+}
